2fffb0b80519b50314de80c2c2f88b15b4780dd3
amine <labassiamine@gmail.com>
2019-11-03 22:51:14 +0100

    os share

---
+++ app/src/main/java/com/bayardpresse/morteleadele/android/FileProvider.java
@@ -0,0 +1,4 @@
+package com.bayardpresse.morteleadele.android;
+
+public class FileProvider extends androidx.core.content.FileProvider {
+}
--- app/src/main/java/com/bayardpresse/morteleadele/android/StickersGridAdapter.java
+++ app/src/main/java/com/bayardpresse/morteleadele/android/StickersGridAdapter.java
@@ -1,16 +1,26 @@
 package com.bayardpresse.morteleadele.android;
 
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.bayardpresse.morteleadele.android.model.Pack;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class StickersGridAdapter extends BaseAdapter {
 
@@ -50,12 +60,42 @@ public class StickersGridAdapter extends BaseAdapter {
             imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
             imageView.setPadding(8, 8, 8, 8);
             imageView.setAdjustViewBounds(true);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        ImageView iv = (ImageView) v;
+                        Bitmap stickerBmp = BitmapFactory.decodeResource(v.getContext().getResources(), (Integer) v.getTag());
+                        ContextWrapper wrapper = new ContextWrapper(v.getContext());
+                        File file = wrapper.getDir("images", Context.MODE_PRIVATE);
+                        file = new File(file, "image.webp");
+                        OutputStream stream = new FileOutputStream(file);
+                        stickerBmp.compress(Bitmap.CompressFormat.WEBP, 100, stream);
+                        stream.flush();
+                        stream.close();
+                        Uri fileUri = Uri.parse(file.getAbsolutePath());
+                        FileInputStream fis = new FileInputStream(fileUri.getPath());
+                        Bitmap bitmap = BitmapFactory.decodeStream(fis);
+                        fis.close();
+                        file = new File(v.getContext().getCacheDir() + "/thestickers.webp");
+                        bitmap.compress(Bitmap.CompressFormat.WEBP, 100, new FileOutputStream(file));
+                        Uri contentUri = FileProvider.getUriForFile(v.getContext(), BuildConfig.FILE_PROVIDER_AUTHORITY, file);
+                        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                        shareIntent.setType("image/*");
+                        shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+                        v.getContext().startActivity(Intent.createChooser(shareIntent, "Partager le sticker"));
+                    } catch (java.io.IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
         else
         {
             imageView = (ImageView) convertView;
         }
         imageView.setImageResource(resID);
+        imageView.setTag(resID);
         return imageView;
     }
 }
